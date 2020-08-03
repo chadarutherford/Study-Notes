62d8c50dc87c7a17d1bce7c411b0bc900043ac0d
Chad Rutherford <chad_rutherford@outlook.com>
2020-08-03 18:14:17 -0400

    Refactored Seed Data

--- Study Notes/Controller/Model Controller/NoteController.swift
+++ Study Notes/Controller/Model Controller/NoteController.swift
@@ -9,17 +9,18 @@ import Foundation
 
 class NoteController {
 	init() {
-		Note(answer: "Nucleic Acids", clues: NSOrderedSet(array: [
-			Clue(text: "A complex organic substance present in living cells"),
-			Clue(text: "Includes: RNA & DNA"),
-			Clue(text: "Carry out several cellular processes. Especially involved in regulation and expression of genes.")
-		] as! [Clue]))
-		Note(answer: "DNA", clues: NSOrderedSet(array: [
-			Clue(text: "A polymer (substance that has a molecular structure consisting of a large number of similar units bonded together. E.g. Protein, cellulose, Nucleic Acids"),
-			Clue(text: "Made from a long string of repeating units called nucleotides"),
-			Clue(text: "Stores biological information"),
-			Clue(text: "Is the hereditary material in all living organisms")
-		] as! [Clue] ))
+		Category(title: "Basic Cell Structure", notes: [
+					Note(answer: "Nucleic Acids", clues: NSOrderedSet(array: [
+						Clue(text: "A complex organic substance present in living cells"),
+						Clue(text: "Includes: RNA & DNA"),
+						Clue(text: "Carry out several cellular processes. Especially involved in regulation and expression of genes.")
+					] as! [Clue]))!,
+					Note(answer: "DNA", clues: NSOrderedSet(array: [
+						Clue(text: "A polymer (substance that has a molecular structure consisting of a large number of similar units bonded together. E.g. Protein, cellulose, Nucleic Acids"),
+						Clue(text: "Made from a long string of repeating units called nucleotides"),
+						Clue(text: "Stores biological information"),
+						Clue(text: "Is the hereditary material in all living organisms")
+					] as! [Clue] ))! ])
 		try! CoreDataStack.shared.save()
 	}
 }
