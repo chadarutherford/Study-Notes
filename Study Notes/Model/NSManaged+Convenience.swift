59b67ae3c5f40a2e18b1ee51d7ab0671d504a824
Chad Rutherford <chad_rutherford@outlook.com>
2020-08-03 18:14:44 -0400

    Changed Note+Convenience to NSManaged+Convenience

--- Study Notes/Model/NSManaged+Convenience.swift
+++ Study Notes/Model/NSManaged+Convenience.swift
@@ -8,6 +8,14 @@
 import CoreData
 import Foundation
 
+extension Category {
+	@discardableResult convenience init?(title: String, notes: NSOrderedSet, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+		self.init(context: context)
+		self.title = title
+		self.notes = notes
+	}
+}
+
 extension Note {
 	@discardableResult convenience init?(answer: String,
 										 clues: NSOrderedSet,
