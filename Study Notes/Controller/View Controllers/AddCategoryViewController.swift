c52c3da168ac198b7e3fe4699ff368a4700657b3
Chad Rutherford <chad_rutherford@outlook.com>
2020-08-04 11:23:08 -0400

    Added AddCategoryVC and Empty View

---
+++ Study Notes/Controller/View Controllers/AddCategoryViewController.swift
@@ -0,0 +1,134 @@
+//
+//  AddCategoryViewController.swift
+//  Study Notes
+//
+//  Created by Chad Rutherford on 8/3/20.
+//
+
+import SwiftUI
+import UIKit
+
+class AddCategoryViewController: UIViewController {
+	
+	let containerView: UIView = {
+		let containerView = UIView()
+		containerView.translatesAutoresizingMaskIntoConstraints = false
+		containerView.backgroundColor = .systemBackground
+		containerView.layer.cornerRadius = 15
+		containerView.clipsToBounds = true
+		return containerView
+	}()
+	
+	let label: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "Category Name:"
+		label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+		label.textColor = .label
+		return label
+	}()
+	
+	let categoryTextField: UITextField = {
+		let textField = UITextField()
+		textField.translatesAutoresizingMaskIntoConstraints = false
+		textField.borderStyle = .roundedRect
+		textField.placeholder = "Category Name:"
+		return textField
+	}()
+	
+	let cancelButton: UIButton = {
+		let button = UIButton()
+		button.translatesAutoresizingMaskIntoConstraints = false
+		let attributes = [
+			NSAttributedString.Key.foregroundColor : UIColor.label,
+			NSAttributedString.Key.font : UIFont.systemFont(ofSize: 18, weight: .semibold)
+		]
+		button.layer.borderColor = UIColor.label.cgColor
+		button.layer.borderWidth = 2
+		button.layer.cornerRadius = 10
+		button.layer.masksToBounds = true
+		button.setAttributedTitle(NSAttributedString(string: "Cancel", attributes: attributes), for: .normal)
+		button.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
+		return button
+	}()
+	
+	let saveButton: UIButton = {
+		let button = UIButton()
+		button.translatesAutoresizingMaskIntoConstraints = false
+		let attributes = [
+			NSAttributedString.Key.foregroundColor : UIColor.label,
+			NSAttributedString.Key.font : UIFont.systemFont(ofSize: 18, weight: .semibold)
+		]
+		button.layer.borderColor = UIColor.label.cgColor
+		button.layer.borderWidth = 2
+		button.layer.cornerRadius = 10
+		button.layer.masksToBounds = true
+		button.setAttributedTitle(NSAttributedString(string: "Save", attributes: attributes), for: .normal)
+		button.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+		return button
+	}()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		configureUI()
+	}
+	
+	private func configureUI() {
+		view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+		view.addSubview(containerView)
+		containerView.addSubview(label)
+		containerView.addSubview(categoryTextField)
+		containerView.addSubview(saveButton)
+		containerView.addSubview(cancelButton)
+		NSLayoutConstraint.activate([
+			containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120),
+			containerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+			containerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+			containerView.bottomAnchor.constraint(equalTo: saveButton.bottomAnchor, constant: 8),
+			
+			label.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
+			label.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8),
+			
+			categoryTextField.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 8),
+			categoryTextField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8),
+			categoryTextField.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -8),
+			categoryTextField.heightAnchor.constraint(equalToConstant: 40),
+			
+			saveButton.topAnchor.constraint(equalTo: categoryTextField.bottomAnchor, constant: 8),
+			saveButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -40),
+			saveButton.heightAnchor.constraint(equalToConstant: 40),
+			saveButton.leadingAnchor.constraint(equalTo: containerView.centerXAnchor, constant: 40),
+			
+			cancelButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 40),
+			cancelButton.trailingAnchor.constraint(equalTo: containerView.centerXAnchor, constant: -40),
+			cancelButton.topAnchor.constraint(equalTo: categoryTextField.bottomAnchor, constant: 8),
+			cancelButton.heightAnchor.constraint(equalToConstant: 40)
+		])
+	}
+	
+	@objc private func cancelButtonTapped() {
+		dismiss(animated: true)
+	}
+	
+	@objc private func saveButtonTapped() {
+		guard let title = categoryTextField.text, !title.isEmpty else { return }
+		Category(title: title, notes: [])
+		try! CoreDataStack.shared.save()
+	}
+}
+
+struct AddCategoryPreview: PreviewProvider {
+	static var previews: some View {
+		ContainerView().edgesIgnoringSafeArea(.all)
+	}
+	
+	struct ContainerView: UIViewControllerRepresentable {
+		func updateUIViewController(_ uiViewController: AddCategoryPreview.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<AddCategoryPreview.ContainerView>) {
+			
+		}
+		
+		func makeUIViewController(context: UIViewControllerRepresentableContext<AddCategoryPreview.ContainerView>) -> UIViewController {
+			return AddCategoryViewController()
+		}
+	}
+}
---
+++ Study Notes/View/Custom Views/EmptyView.swift
@@ -0,0 +1,41 @@
+//
+//  EmptyView.swift
+//  Study Notes
+//
+//  Created by Chad Rutherford on 8/3/20.
+//
+
+import UIKit
+
+class EmptyView: UIView {
+	
+	let emptyLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.font = UIFont.boldSystemFont(ofSize: 25)
+		label.textColor = .black
+		label.numberOfLines = 0
+		label.text = "There are no cards in this deck, click the add button to add cards"
+		return label
+	}()
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		configureUI()
+	}
+	
+	@available(*, unavailable)
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+		configureUI()
+	}
+	
+	private func configureUI() {
+		addSubview(emptyLabel)
+		
+		NSLayoutConstraint.activate([
+			emptyLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+			emptyLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+		])
+	}
+}
