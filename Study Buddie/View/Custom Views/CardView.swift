ab6a7987185aa4ed7aa799a8dcfc292a3704c6fb
Chad Rutherford <chad_rutherford@outlook.com>
2020-08-07 15:59:43 -0400

    Refactored CardView to look like an actual index card.

--- Study Buddie/View/Custom Views/CardView.swift
+++ Study Buddie/View/Custom Views/CardView.swift
@@ -1,19 +1,24 @@
 //
-// CardView.swift
-// Study Notes
+//  CardView2.swift
+//  Study Buddie
 //
-// Created by Chad Rutherford on 8/3/20.
-// 
+//  Created by Chad Rutherford on 8/7/20.
 //
 
 import UIKit
 
 class CardView: UIView {
 	
+	var isFaceUp: Bool = true {
+		didSet {
+			setNeedsDisplay()
+		}
+	}
+	
 	let answerLabel: UILabel = {
 		let label = UILabel()
 		label.translatesAutoresizingMaskIntoConstraints = false
-		label.textColor = .label
+		label.textColor = .black
 		label.font = UIFont.boldSystemFont(ofSize: 25)
 		return label
 	}()
@@ -51,5 +56,31 @@ class CardView: UIView {
 			cluesStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
 			cluesStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
 		])
+	}
+	
+	override func draw(_ rect: CGRect) {
+		if isFaceUp {
+			return
+		} else {
+			let originX = rect.origin.x
+			let originY = rect.origin.y
+			
+			guard let context = UIGraphicsGetCurrentContext() else { return }
+			context.beginPath()
+			for i in 0 ..< Int(rect.size.height) {
+				context.setLineWidth(2)
+				if i == 0 {
+					context.move(to: CGPoint(x: originX, y: originY + 40.0))
+					context.addLine(to: CGPoint(x: rect.size.width, y: originY + 40.0))
+					context.setStrokeColor(UIColor.indexCardRed.cgColor)
+					context.strokePath()
+				} else {
+					context.move(to: CGPoint(x: originX, y: originY + CGFloat((i + 1) * 40)))
+					context.addLine(to: CGPoint(x: rect.size.width, y: originY + CGFloat((i + 1) * 40)))
+					context.setStrokeColor(UIColor.indexCardBlue.cgColor)
+					context.strokePath()
+				}
+			}
+		}
 	}
 }
