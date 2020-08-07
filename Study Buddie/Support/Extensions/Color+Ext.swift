d232c732797c4a7da397b86f114de003ead1abf5
Chad Rutherford <chad_rutherford@outlook.com>
2020-08-07 16:07:47 -0400

    Created colors for the Index Card

---
+++ Study Buddie/Support/Extensions/Color+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Ext.swift
+//  Study Buddie
+//
+//  Created by Chad Rutherford on 8/7/20.
+//
+
+import UIKit
+
+extension UIColor {
+	static let indexCardRed = UIColor(named: "IndexCardRed")!
+	static let indexCardBlue = UIColor(named: "IndexCardBlue")!
+}
